21e41303fda6fa7de8d8d9430ac0d9f80d69b3c5
Erlan <58254625+ErlanMak@users.noreply.github.com>
2020-12-20 03:09:53 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.querySelector('.burger-wrapper').addEventListener('click', function () {
+	document.querySelector('.burger-menu').classList.toggle('burger-menu-active')
+	document.querySelector('.header__nav').classList.toggle('nav-active')
+
+});
